e8ca02e8b3b05153517f8e11284126a696a46ae7
sanbei011 <sanbei101@outlook.com>
2024-08-03 15:10:30 +0800

    持久化逻辑重构

--- lib/service/auth/auth_gate.dart
+++ lib/service/auth/auth_gate.dart
@@ -12,59 +12,48 @@ class AuthGate extends StatefulWidget {
 }
 
 class AuthGateState extends State<AuthGate> {
-  bool _isLoggedIn = false; // 用于存储用户是否已登录的状态
+  late Future<bool> _isLoggedInFuture;
 
   @override
   void initState() {
     super.initState();
-    _checkLoginStatus(); // 初始化时检查登录状态
+    _isLoggedInFuture = _checkLoginStatus();
   }
 
-  // 检查用户的登录状态并保存登录状态
-  Future<void> _checkLoginStatus() async {
-    SharedPreferences prefs =
-        await SharedPreferences.getInstance(); // 获取SharedPreferences实例
-    setState(() {
-      _isLoggedIn =
-          prefs.getBool('isLoggedIn') ?? false; // 获取存储的登录状态，如果没有则默认为false
-    });
-
-    if (_isLoggedIn) {
-      final user = Supabase.instance.client.auth.currentUser; // 获取当前Supabase用户
-      if (user == null) {
-        setState(() {
-          _isLoggedIn = false; // 如果Supabase中没有用户信息，则更新状态为未登录
-          prefs.setBool('isLoggedIn', false); // 同时更新SharedPreferences中的登录状态
-        });
-      }
+  Future<bool> _checkLoginStatus() async {
+    final session = Supabase.instance.client.auth.currentSession;
+    if (session != null) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setBool('isLoggedIn', true);
+      return true;
+    } else {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setBool('isLoggedIn', false);
+      return false;
     }
   }
 
-  // 用户登出的方法
   Future<void> _signOut() async {
-    await Supabase.instance.client.auth.signOut(); // 调用Supabase的signOut方法
+    await Supabase.instance.client.auth.signOut();
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool('isLoggedIn', false); // 更新SharedPreferences中的登录状态为未登录
+    await prefs.setBool('isLoggedIn', false);
     setState(() {
-      _isLoggedIn = false; // 更新组件状态
+      _isLoggedInFuture = Future.value(false);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder<AuthState>(
-        stream: Supabase.instance.client.auth.onAuthStateChange, // 监听认证状态变化的流
+      body: FutureBuilder<bool>(
+        future: _isLoggedInFuture,
         builder: (context, snapshot) {
-          if (snapshot.hasData &&
-              snapshot.data!.event == AuthChangeEvent.signedIn) {
-            SharedPreferences.getInstance().then(
-                (prefs) => prefs.setBool('isLoggedIn', true)); // 如果用户已登录，保存登录状态
-            return HomePage(signOut: _signOut); // 返回主页，并传递登出方法
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasData && snapshot.data == true) {
+            return HomePage(signOut: _signOut);
           } else {
-            SharedPreferences.getInstance().then((prefs) =>
-                prefs.setBool('isLoggedIn', false)); // 如果用户未登录，保存未登录状态
-            return const LoginOrRegister(); // 返回登录或注册页面
+            return const LoginOrRegister();
           }
         },
       ),
